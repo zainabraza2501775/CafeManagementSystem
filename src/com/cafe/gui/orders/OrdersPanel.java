d285a2eebc66db2015aeac260fee13d19982d5c9
MS LAPTOP <MS LAPTOP@DESKTOP-Q1KG0VQ>
2026-05-31 03:03:37 +0500

    Created Initial Application Architecture

---
+++ src/com/cafe/gui/orders/OrdersPanel.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cafe.gui.orders;
+
+/**
+ *
+ * @author MS LAPTOP
+ */
+public class OrdersPanel {
+    
+}
